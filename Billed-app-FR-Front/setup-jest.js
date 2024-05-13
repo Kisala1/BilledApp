0a1835854ac21b0d39293eaba753e5c8136a46c7
Aude <alexandre.aude16@gmail.com>
2024-05-13 11:59:54 +0200

    Correction bug Dashboard

--- Billed-app-FR-Front/setup-jest.js
+++ Billed-app-FR-Front/setup-jest.js
@@ -1,6 +1,3 @@
 import $ from 'jquery';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 
 global.$ = global.jQuery = $;
-configure({ adapter: new Adapter() });
--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -76,6 +76,10 @@ export default class {
     this.document = document;
     this.onNavigate = onNavigate;
     this.store = store;
+    this.counter = {}
+    //this.counterShow = {}
+    //this.counterEdit = {}
+    //this.ids = {}
     $('#arrow-icon1').click((e) => this.handleShowTickets(e, bills, 1));
     $('#arrow-icon2').click((e) => this.handleShowTickets(e, bills, 2));
     $('#arrow-icon3').click((e) => this.handleShowTickets(e, bills, 3));
@@ -95,10 +99,9 @@ export default class {
   };
 
   handleEditTicket(e, bill, bills) {
-    if (this.counter === undefined || this.id !== bill.id) this.counter = 0;
-    if (this.id === undefined || this.id !== bill.id) this.id = bill.id;
+    if (this.counter === undefined || this.id !== bill.id) this.counter = 0
+    if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     if (this.counter % 2 === 0) {
-      console.log('Edit ticket this.counter : ' + this.counter);
       bills.forEach((b) => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' });
       });
@@ -107,7 +110,6 @@ export default class {
       $('.vertical-navbar').css({ height: '150vh' });
       this.counter++;
     } else {
-      console.log('Edit ticket else this.counter : ' + this.counter);
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' });
 
       $('.dashboard-right-container div').html(`
@@ -116,6 +118,7 @@ export default class {
       $('.vertical-navbar').css({ height: '120vh' });
       this.counter++;
     }
+
     $('#icon-eye-d').click(this.handleClickIconEye);
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill));
     $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill));
@@ -142,23 +145,24 @@ export default class {
   };
 
   handleShowTickets(e, bills, index) {
-    if (this.counter === undefined || this.index !== index) this.counter = 0;
-    if (this.index === undefined || this.index !== index) this.index = index;
-
+    if (this.counter === undefined || this.index !== index) this.counter = 0
+    if (this.index === undefined || this.index !== index) this.index = index
+    
     if (this.counter % 2 === 0) {
-      console.log('Voir le ticket this.counter : ' + this.counter);
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)' });
       $(`#status-bills-container${this.index}`).html(
         cards(filteredBills(bills, getStatus(this.index)))
       );
       this.counter++;
     } else {
-      console.log('Voir le ticket else this.counter : ' + this.counter);
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)' });
       $(`#status-bills-container${this.index}`).html('');
       this.counter++;
     }
+    
+
     bills.forEach((bill) => {
+      $(`#open-bill${bill.id}`).off('click');
       $(`#open-bill${bill.id}`).click((e) =>
         this.handleEditTicket(e, bill, bills)
       );
